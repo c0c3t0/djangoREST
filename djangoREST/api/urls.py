7f9173d3eda379280d3c77440d347a77d6a0c7ca
c0c3t0 <stoyanka.stoichkova@abv.bg>
2022-05-23 21:23:18 +0300

    API generics views

--- djangoREST/api/urls.py
+++ djangoREST/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from djangoREST.api.views import ProductsListView
+from djangoREST.api.views import ProductsListView, SingleProductView
 
 urlpatterns = [
-    path('products/', ProductsListView.as_view(), name='product list')
+    path('products/', ProductsListView.as_view(), name='product list'),
+    path('products/<int:pk>', SingleProductView.as_view(), name='product'),
 ]
--- djangoREST/api/views.py
+++ djangoREST/api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import serializers
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 
 from djangoREST.api.models import Product
 
@@ -12,17 +13,29 @@ class ProductSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ProductsListView(APIView):
-    def get(self, request):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
+# class ProductsListView(APIView):
+#     def get(self, request):
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#
+#         return Response(data=serializer.data)
+#
+#     def post(self, request):
+#         serializer = ProductSerializer(data=request.data)
+#         if serializer.is_valid():
+#             print(serializer.validated_data)
+#             serializer.save()
+#             return Response(status=201)
+#         return Response(serializer.errors, status=400)
 
-        return Response(data=serializer.data)
+# class ProductsListView(ListAPIViewistAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            print(serializer.validated_data)
-            serializer.save()
-            return Response(status=201)
-        return Response(serializer.errors, status=400)
+class ProductsListView(ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class SingleProductView(RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
